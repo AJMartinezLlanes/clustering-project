7dc899dc31cc0fe99dafd11da4f07693c05f300d
AJ Martinez <96085262+AJMartinezLlanes@users.noreply.github.com>
2022-04-07 18:02:45 -0500

    wrangle

--- wrangle.py
+++ wrangle.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+import os
 
 import env
 
@@ -16,96 +16,113 @@ def get_db_url(database):
     url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
     return url
 
-
-# acquire zillow data using the query
-def get_zillow(sql):
-    sql = '''
-    SELECT prop.*, 
-           pred.logerror, 
-           pred.transactiondate, 
-           air.airconditioningdesc, 
-           arch.architecturalstyledesc, 
-           build.buildingclassdesc, 
-           heat.heatingorsystemdesc, 
-           landuse.propertylandusedesc, 
-           story.storydesc, 
-           construct.typeconstructiondesc 
-    FROM   properties_2017 prop  
-           INNER JOIN (SELECT parcelid,
-       					  logerror,
-                          Max(transactiondate) transactiondate 
-                       FROM   predictions_2017 
-                       GROUP  BY parcelid, logerror) pred
-                    USING (parcelid) 
-           LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
-           LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
-           LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
-           LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
-           LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
-           LEFT JOIN storytype story USING (storytypeid) 
-           LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
-    WHERE  prop.latitude IS NOT NULL 
-           AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31' 
-    '''
+def get_zillow_data():    
     
-    url = get_db_url('zillow')
-    zillow_df = pd.read_sql(sql, url, index_col='id')
+    '''This function will acquire data from zillow using env file and rename the columns before saving it as CSV'''
+
+    filename = 'zillow.csv'
+    if os.path.exists(filename):
+        print('Reading from csv file...')
+        return pd.read_csv(filename)
+      
+    query = '''
+        SELECT prop.*,
+	    	   pred.logerror,
+		       pred.transactiondate,	
+    		   cons.typeconstructiondesc,
+	    	   air.airconditioningdesc,
+		       arch.architecturalstyledesc,
+    		   build.buildingclassdesc,
+	    	   land.propertylandusedesc,		
+		       story.storydesc
+    	FROM properties_2017 prop
+	    	INNER JOIN(SELECT parcelid, logerror, MAX(transactiondate)transactiondate
+		    			FROM predictions_2017
+			    		GROUP BY parcelid, logerror) pred
+				    USING (parcelid)
+    		LEFT JOIN typeconstructiontype cons USING (typeconstructiontypeid)
+	    	LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
+		    LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+    		LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+	    	LEFT JOIN propertylandusetype land USING (propertylandusetypeid)
+		    LEFT JOIN storytype story USING (storytypeid)
+	    WHERE prop.latitude IS NOT NULL
+		    AND prop.longitude IS NOT NULL
+    		AND transactiondate LIKE '2017%%';
+        '''
+    print('Getting a fresh copy from SQL database...')
+    df = pd.read_sql(query, get_db_url('zillow'))
     
-    return zillow_df
-
-
-def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70):
-	#function that will drop rows or columns based on the percent of values that are missing:\
-	#handle_missing_values(df, prop_required_column, prop_required_row
-    threshold = int(round(prop_required_column*len(df.index),0))
-    df.dropna(axis=1, thresh=threshold, inplace=True)
-    threshold = int(round(prop_required_row*len(df.columns),0))
-    df.dropna(axis=0, thresh=threshold, inplace=True)
+	# transfer dataframe into a csv for faster access
+	print('Saving to csv...')
+    df.to_csv(filename, index=False)
     return df
 
+# Function returns a dataframe with number of rows missing in a column 
+# and percentage of the column missing
+def nulls_by_col(df):
+    '''
+    This function will take a dataframe and 
+    return a dataframe with null values and percentage by column 
+    '''
+    num_rows_missing = df.isnull().sum()
+    pct_rows_missing = num_rows_missing/num_rows
+    df_missing = pd.DataFrame({'number_missing_rows': num_rows_missing, 'percent_rows_missing': pct_rows_missing})
+    return df_missing
 
+# Function returns a dataframe with number of rows missing 
+# and percentage of rows missing
+def nulls_by_row(df):
+    '''
+    This function will take a dataframe and 
+    return a dataframe with null values and percentage by row 
+    '''
+    miss_df = pd.DataFrame(df.isna().sum(axis =1), columns = ['num_cols_missing']).reset_index().groupby('num_cols_missing')
+    miss_df = miss_df.count().reset_index().rename(columns = {'index': 'num_rows' })
+   
+    miss_df['pct_cols_missing'] = miss_df.num_cols_missing/df.shape[1]
+    return miss_df
+
+# This function handles the missing values by removing columns and rows with 50% nulls
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .5):
+    '''
+    function that will drop rows or columns based on the 
+    percent of values that are missing
+    '''
+    n_required_column = round(df.shape[0] * prop_required_column)
+    n_required_row = round(df.shape[1] * prop_required_row)
+    df = df.dropna(axis=0, thresh=n_required_row)
+    df = df.dropna(axis=1, thresh=n_required_column)
+    return df  
+
+
+# This function will take in a dataframe and the columns to be removed
 def remove_columns(df, cols_to_remove):  
-	#remove columns not needed
+	'''
+    This function will return a dataframe with indicated columns removed
+    '''
     df = df.drop(columns=cols_to_remove)
     return df
 
+# This function will remove some extreme outliers based on observation instead of IQR
+def remove_outliers(df):
+    '''
+    remove outliers in bathroomcnt, bedroomcnt, calculatedfinishedsquarefeet, lotsizesquarefeet,
+    structuretaxvaluedollarcnt, taxvaluedollarcnt, landtaxvaluedollarcnt, taxamount
+    '''
 
-def wrangle_zillow():
-    df = pd.read_csv('zillow.csv')
-    
-    # Restrict df to only properties that meet single unit use criteria
-    single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
-    df = df[df.propertylandusetypeid.isin(single_use)]
-    
-    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
-    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
-            & (df.calculatedfinishedsquarefeet>350)]
-
-    # Handle missing values i.e. drop columns and rows based on a threshold
-    df = handle_missing_values(df)
-    
-    # Add column for counties
-    df['county'] = np.where(df.fips == 6037, 'Los_Angeles',
-                           np.where(df.fips == 6059, 'Orange', 
-                                   'Ventura'))    
-    # drop columns not needed
-    df = remove_columns(df, ['id',
-       'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid'
-       ,'propertycountylandusecode', 'propertylandusetypeid','propertyzoningdesc', 
-        'censustractandblock', 'propertylandusedesc','unitcnt'
-                            ,'buildingqualitytypeid'])
-    
-    # replace nulls with median values for select columns
-    df.lotsizesquarefeet.fillna(7313, inplace = True)
-
-    # Columns to look for outliers
-    df = df[df.taxvaluedollarcnt < 5_000_000]
-    df[df.calculatedfinishedsquarefeet < 8000]
-    
-    # Just to be sure we caught all nulls, drop them here
-    df = df.dropna()
-    
-    return df
+    return df[((df.bathroomcnt <= 7) & (df.bedroomcnt <= 7) &  
+               (df.bathroomcnt > 0) & 
+               (df.bedroomcnt > 0) & 
+               (df.calculatedfinishedsquarefeet > 350) & 
+               (df.calculatedfinishedsquarefeet < 6000) & 
+               (df.lotsizesquarefeet < 500_000) &
+               (df.regionidzip < 100_000) &
+               (df.structuretaxvaluedollarcnt < 1_000_000) &
+               (df.taxvaluedollarcnt < 2_000_000) &
+               (df.landtaxvaluedollarcnt < 1_500_000) &
+               (df.taxamount < 30_000)
+              )]
 
 
 def min_max_scaler(train, valid, test):
@@ -122,61 +139,40 @@ def min_max_scaler(train, valid, test):
     return scaler, train, valid, test
 
 
-def get_mall_customers(sql):
-	    url = get_db_url('mall_customers')
-	    mall_df = pd.read_sql(sql, url, index_col='customer_id')
-	    return mall_df
-
-
-def wrangle_mall_df():
-    
-    # acquire data
-    sql = 'select * from customers'
-
-
-    # acquire data from SQL server
-    mall_df = get_mall_customers(sql)
-    
-    # handle outliers
-    mall_df = remove_outliers(mall_df, 1.5, ['age', 'spending_score', 'annual_income'])
-    
-    # get dummy for gender column
-    dummy_df = pd.get_dummies(mall_df.gender, drop_first=True)
-    mall_df = pd.concat([mall_df, dummy_df], axis=1).drop(columns = ['gender'])
-    mall_df.rename(columns= {'Male': 'is_male'}, inplace = True)
-    # return mall_df
-    return mall_df
-
-
-def remove_outliers(df, k, col_list):
-    ''' remove outliers from a list of columns in a dataframe 
-        and return that dataframe
+# This function takes all the small functions and combines them to get a clean zillow dataframe
+def wrangle_zillow(df):
     '''
-    # Create a column that will label our rows as containing an outlier value or not
-    num_obs = df.shape[0]
-    df['outlier'] = False
-    for col in col_list:
+    This function takes in a fresh copy of zillow data and cleans it. 
+    After this it will be easy to split
+    '''
+    # Handle missing values i.e. drop columns and rows based on a threshold
+    df = handle_missing_values(df)
         
-        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
-        
-        iqr = q3 - q1   # calculate interquartile range
-        
-        upper_bound = q3 + k * iqr   # get upper bound
-        lower_bound = q1 - k * iqr   # get lower bound
-
-        # update the outlier label any time that the value is outside of boundaries
-        df['outlier'] = np.where(((df[col] < lower_bound) | (df[col] > upper_bound)) & (df.outlier == False), True, df.outlier)
+    # drop columns not needed
+    cols_to_remove = ['id', 'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'roomcnt','propertycountylandusecode', 
+    'propertylandusetypeid', 'censustractandblock', 'rawcensustractandblock', 'assessmentyear']
     
-    df = df[df.outlier == False]
-    df.drop(columns=['outlier'], inplace=True)
-    print(f"Number of observations removed: {num_obs - df.shape[0]}")
+    df = remove_columns(df, cols_to_remove)
+    
+    # Columns to look for outliers
+    df = remove_outliers(df)
+    
+    # Add column for counties
+    df['county'] = np.where(df.fips == 6037, 'Los_Angeles', np.where(df.fips == 6059, 'Orange', 'Ventura'))    
 
+    # replace nulls with median values for select columns
+    lot_median = df.lotsizesquarefeet.median()
+    df.lotsizesquarefeet.fillna(lot_median, inplace = True)
+
+    # Just to be sure we caught all nulls, drop them here
+    df = df.dropna()
     return df
 
-
+# 
 def split_data(df):
-
-    ''' this function will take your raw data frame, clean it and split it'''
+    ''' 
+    This function will take your clean dataframe and split it
+    '''
     
     # split the data
     train_validate, test = train_test_split(df, test_size=.2, random_state=177)
